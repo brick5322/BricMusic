60ddc5aa116447e8e1b8013043e019d4999c154a
bric <brick5322@outlook.com>
2022-06-07 00:39:20 +0800

    WIN环境安装包

---
+++ BricMusic/win/BricMusic/Language/zh_CN.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>QAction</name>
+    <message>
+        <location filename="main.cpp" line="136"/>
+        <source>Show</source>
+        <translatorcomment>显示</translatorcomment>
+        <translation>显示</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="137"/>
+        <source>Exit</source>
+        <translatorcomment>退出</translatorcomment>
+        <translation>退出</translation>
+    </message>
+</context>
+<context>
+    <name>QMessageBox</name>
+    <message>
+        <location filename="main.cpp" line="73"/>
+        <source>Lua File Err!</source>
+        <translatorcomment>Lua文件错误</translatorcomment>
+        <translation>Lua文件错误！</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="74"/>
+        <source>You might have a Lua script with an error or you might even have deleted the script.
+Anyway,see Readme.md(Windows) or try &quot;man BricMusic&quot;(Linux) to find solution.</source>
+        <translation>你可能时搞了个错误的Lua脚本，又或者，你直接把那个脚本删掉了。
+总之，你可以先看看Readme.md(Windows)或者试试“man BricMusic”（Linux）。</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="107"/>
+        <source>Load Err!</source>
+        <translation>音乐文件加载错误！</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="108"/>
+        <source>You load NOTHING!!!!
+Anyway,see Readme.md(Windows) or try &quot;man BricMusic&quot;(Linux) to find solution.</source>
+        <translation>你啥都没加进来，这可放不了。
+总之，你可以先看看Readme.md(Windows)或者试试“man BricMusic”（Linux）。</translation>
+    </message>
+</context>
+</TS>
